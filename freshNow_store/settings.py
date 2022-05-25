7b2785e20b20a9543e8cd3eb6d329c6ac556f6dc
KonstantinaStrantzali <konstrantz@gmail.com>
2022-05-25 09:59:12 +0000

    add to bag functionality

--- freshNow_store/settings.py
+++ freshNow_store/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'django.contrib.messages.context_processors.messages',
                 'products.contexts.all_categories',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -151,3 +152,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
