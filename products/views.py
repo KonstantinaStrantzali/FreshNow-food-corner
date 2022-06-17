d28ffdb2a44a4d87731af3620b2e9b137c5fb405
KonstantinaStrantzali <konstrantz@gmail.com>
2022-06-17 09:48:32 +0000

    Admin Management - fixing image fields

--- products/views.py
+++ products/views.py
@@ -82,7 +82,7 @@ def add_product(request):
         if form.is_valid():
             product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('product_details', args=[product.id]))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -126,6 +126,7 @@ def delete_product(request, product_id):
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
+
     """ Delete a product from the store """
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
