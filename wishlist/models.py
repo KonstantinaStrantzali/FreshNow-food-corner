d4f2308c92a203e7936730e01c92dbcda93c3796
KonstantinaStrantzali <konstrantz@gmail.com>
2022-06-26 12:20:13 +0000

    migrate models and register them in admin panel

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,11 +1,17 @@
 from django.db import models
-from profiles.models import userprofile
-from products.models import Product
-
-
 from profiles.models import UserProfile
 from products.models import Product
 
+
 # Create your models here.
 
-user = 
+class Wishlist(models.Model):
+
+    """
+    Model to show all product items within the users wishlist
+    """
+    profile_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return f'wishlist ({self.profile_user})'
