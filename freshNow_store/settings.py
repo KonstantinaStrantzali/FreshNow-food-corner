87023a15f346273fcad3e388f256289271f19288
KonstantinaStrantzali <konstrantz@gmail.com>
2022-07-03 11:14:15 +0000

    testing on Debug functionality

--- freshNow_store/settings.py
+++ freshNow_store/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = os.environ.get('DEBUG')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
