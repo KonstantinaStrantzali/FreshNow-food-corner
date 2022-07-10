50cf5a3d61b8b36aaebbd28cbd869cb1549bc8fc
KonstantinaStrantzali <konstrantz@gmail.com>
2022-07-10 17:46:56 +0000

    added google font

--- products/views.py
+++ products/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from .models import Product, Category
 from .forms import ProductForm
 from django.db.models import Avg
-
+from django.db.models import Q
 from reviews.models import Reviews
 from reviews.forms import ReviewForm
 from profiles.models import UserProfile
@@ -49,8 +51,7 @@ def all_products(request):
                     request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
 
-            queries = Q(name__icontains=query) | Q(
-                description__icontains=query)
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
